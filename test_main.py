4308b8fd6faf8e4126fde3863f59c85fb6c4189f
Marcelo Viana <marcelofv12@hotmail.com>
2023-11-29 09:36:21 -0300

    adicionados mais testes

--- test_main.py
+++ test_main.py
@@ -1,6 +1,13 @@
 from starlette.testclient import TestClient
 from main import app
 import pytest
+import json
+import string
+import random
+
+def random_string_gen(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 client = TestClient(app)
 
@@ -12,32 +19,43 @@ def test_fill_gallery():
         assert a in response.json()
 
 
+def test_unauthorize_add_breed():
+    r = client.post("/breeds/new", data=add_breed_data)
+    assert r.status_code == 401
+
+
+def test_add_breed():
+    r = client.post(
+        "/breeds/new",
+        data=json.dumps(add_breed_data),
+        headers=admin_auth_header,
+    )
+    assert r.status_code == 200
+    assert r.json()['name'] == add_breed_data['name']
+    assert r.json()['id'] > 0
+
+
+def test_duplicate_add_breed():
+    r = client.post(
+        "/breeds/new",
+        data=json.dumps(add_breed_data),
+        headers=admin_auth_header,
+    )
+    assert r.status_code == 400
+    assert r.json() == {"msg": "Raça já cadastrada"}
+
+
 def test_list_breeds():
     r = client.get("/breeds/list")
     assert r.status_code == 200
-    for a in [
-        {"name": "Pug", "id": 1},
-        {"name": "Labrador", "id": 2},
-        {"name": "Teeste", "id": 3},
-        {"name": "LULUZI", "id": 4},
-        {"name": "LULUZ2I", "id": 5},
-        {"name": "Lulu da pom", "id": 6},
-    ]:
+    for a in some_available_breeds:
         assert a in r.json()
 
 
 def test_get_kennel():
     r = client.get("/kennels/7")
     assert r.status_code == 200
-    assert r.json() == {
-        "name": "Canil Geize",
-        "phone": "2233299833245199",
-        "instagram": "22dreamp3u2pp23.com.br",
-        "city": {"name": "Vitória da Conquista", "uf": "BA"},
-        "address": "Avenida Olivia Flores",
-        "cep": "39890000",
-        "id": 7,
-    }
+    assert r.json() == kennel7
 
 
 def test_unauth_list_puppies_from_kennel():
@@ -46,92 +64,40 @@ def test_unauth_list_puppies_from_kennel():
 
 
 def test_list_puppies_from_kennel():
-    r = client.get(
-        "/kennels/4/puppies/",
-        headers={
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYXJjZWxvQGVtYWlsLmNvbSIsImV4cCI6MTcwMzQ1MTc2NH0.7H0tsroOtGhRmoixujPCqOb5w7fIB8YjTRkEnN88XCI"
-        },
-    )
+    r = client.get("/kennels/4/puppies/", headers=admin_auth_header)
     assert r.status_code == 200
-    for v in [
-        {
-            "breed": 1,
-            "microchip": False,
-            "minimum_age_departure_in_days": 60,
-            "price": 3000,
-            "gender": -1,
-            "birth": "2023-11-02T18:25:43.511000",
-            "vermifuges": [
-                {"brand": "ENDAL® PLUS - MSD", "date": "2023-11-22T18:25:43.511000"}
-            ],
-            "vaccines": [
-                {"brand": "Bio Max", "type": "V8", "date": "2023-11-23T18:25:43.511000"}
-            ],
-            "id": 29,
-        },
-        {
-            "breed": 1,
-            "microchip": True,
-            "minimum_age_departure_in_days": 60,
-            "price": 1990,
-            "gender": 1,
-            "birth": "2023-11-02T18:25:43.511000",
-            "vermifuges": [
-                {"brand": "ENDAL® PLUS - MSD", "date": "2023-11-22T18:25:43.511000"}
-            ],
-            "vaccines": [
-                {"brand": "Bio Max", "type": "V8", "date": "2023-11-23T18:25:43.511000"}
-            ],
-            "id": 30,
-        },
-        {
-            "breed": 1,
-            "microchip": True,
-            "minimum_age_departure_in_days": 60,
-            "price": 1990,
-            "gender": 1,
-            "birth": "2023-11-02T18:25:43.511000",
-            "vermifuges": [
-                {"brand": "ENDAL® PLUS - MSD", "date": "2023-11-22T18:25:43.511000"}
-            ],
-            "vaccines": [
-                {"brand": "Bio Max", "type": "V8", "date": "2023-11-23T18:25:43.511000"}
-            ],
-            "id": 31,
-        },
-        {
-            "breed": 1,
-            "microchip": True,
-            "minimum_age_departure_in_days": 60,
-            "price": 970,
-            "gender": -1,
-            "birth": "2023-11-02T18:25:43.511000",
-            "vermifuges": [{"brand": "HBO", "date": "2023-11-22T18:25:43.511000"}],
-            "vaccines": [
-                {"brand": "Bio Max", "type": "V8", "date": "2023-11-23T18:25:43.511000"}
-            ],
-            "id": 37,
-        },
-    ]:
+    for v in  puppies_from_kennel4:
         assert v in r.json()
 
 
 def test_empty_list_puppies_from_kennel():
-    r = client.get(
-        "/kennels/2/puppies/",
-        headers={
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYXJjZWxvQGVtYWlsLmNvbSIsImV4cCI6MTcwMzQ1MTc2NH0.7H0tsroOtGhRmoixujPCqOb5w7fIB8YjTRkEnN88XCI"
-        },
-    )
+    r = client.get("/kennels/2/puppies/", headers=admin_auth_header)
     assert r.status_code == 200
     assert r.json() == []
 
 
 def test_get_puppy_from_id():
-    pytest.skip()
     r = client.get("/puppies/2")
     assert r.status_code == 200
-    assert r.json() == {
+    assert r.json() == puppy2
+
+
+admin_auth_header = {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYXJjZWxvQGVtYWlsLmNvbSIsImV4cCI6MTcwMzQ1MTc2NH0.7H0tsroOtGhRmoixujPCqOb5w7fIB8YjTRkEnN88XCI"
+}
+
+add_breed_data = {"name": 'test_{0}'.format(random_string_gen())}
+
+some_available_breeds = [
+        {"name": "Pug", "id": 1},
+        {"name": "Labrador", "id": 2},
+        {"name": "Teeste", "id": 3},
+        {"name": "LULUZI", "id": 4},
+        {"name": "LULUZ2I", "id": 5},
+        {"name": "Lulu da pom", "id": 6},
+    ]
+
+puppy2 = {
         "breed": "Pug",
         "microchip": False,
         "minimum_age_departure_in_days": 60,
@@ -144,6 +110,107 @@ def test_get_puppy_from_id():
         "vaccines": [
             {"brand": "Bio Max", "type": "V8", "date": "2023-11-23T18:25:43.511000"}
         ],
-        "medias": [],
+        # "medias": [],
         "id": 2,
     }
+
+kennel7 = {
+        "name": "Canil Geize",
+        "phone": "2233299833245199",
+        "instagram": "22dreamp3u2pp23.com.br",
+        "city": {"name": "Vitória da Conquista", "uf": "BA"},
+        "address": "Avenida Olivia Flores",
+        "cep": "39890000",
+        "id": 7,
+    }
+
+puppies_from_kennel4 = [
+        {
+            "breed": 1,
+            "microchip": False,
+            "minimum_age_departure_in_days": 60,
+            "price": 3000,
+            "gender": -1,
+            "birth": "2023-11-02T18:25:43.511000",
+            "vermifuges": [
+                {
+                    "brand": "ENDAL® PLUS - MSD",
+                    "date": "2023-11-22T18:25:43.511000",
+                }
+            ],
+            "vaccines": [
+                {
+                    "brand": "Bio Max",
+                    "type": "V8",
+                    "date": "2023-11-23T18:25:43.511000",
+                }
+            ],
+            "id": 29,
+        },
+        {
+            "breed": 1,
+            "microchip": True,
+            "minimum_age_departure_in_days": 60,
+            "price": 1990,
+            "gender": 1,
+            "birth": "2023-11-02T18:25:43.511000",
+            "vermifuges": [
+                {
+                    "brand": "ENDAL® PLUS - MSD",
+                    "date": "2023-11-22T18:25:43.511000",
+                }
+            ],
+            "vaccines": [
+                {
+                    "brand": "Bio Max",
+                    "type": "V8",
+                    "date": "2023-11-23T18:25:43.511000",
+                }
+            ],
+            "id": 30,
+        },
+        {
+            "breed": 1,
+            "microchip": True,
+            "minimum_age_departure_in_days": 60,
+            "price": 1990,
+            "gender": 1,
+            "birth": "2023-11-02T18:25:43.511000",
+            "vermifuges": [
+                {
+                    "brand": "ENDAL® PLUS - MSD",
+                    "date": "2023-11-22T18:25:43.511000",
+                }
+            ],
+            "vaccines": [
+                {
+                    "brand": "Bio Max",
+                    "type": "V8",
+                    "date": "2023-11-23T18:25:43.511000",
+                }
+            ],
+            "id": 31,
+        },
+        {
+            "breed": 1,
+            "microchip": True,
+            "minimum_age_departure_in_days": 60,
+            "price": 970,
+            "gender": -1,
+            "birth": "2023-11-02T18:25:43.511000",
+            "vermifuges": [
+                {
+                    "brand": "HBO",
+                    "date": "2023-11-22T18:25:43.511000",
+                }
+            ],
+            "vaccines": [
+                {
+                    "brand": "Bio Max",
+                    "type": "V8",
+                    "date": "2023-11-23T18:25:43.511000",
+                }
+            ],
+            "id": 37,
+        },
+    ]
