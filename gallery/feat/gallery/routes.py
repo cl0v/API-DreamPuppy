e075842a9cf46574d7f9cd2c0f737733a20aadd5
Marcelo Viana <marcelofv12@hotmail.com>
2023-11-30 12:31:38 -0300

    fix: gallery query params

--- gallery/feat/gallery/routes.py
+++ gallery/feat/gallery/routes.py
@@ -6,9 +6,10 @@ from fastapi import APIRouter, Depends
 router = APIRouter()
 
 
-@router.get("/gallery", response_model=list[GallerySchema])
+@router.get("/gallery/", response_model=list[GallerySchema])
 def fill_gallery(amount: int = 9, db=Depends(get_db)):
     return crud.fill_gallery(db, amount)
 
 
+# @router.get("/gallery", response_model=list[GallerySchema])
 # Dev gallery (Exibir todos os filhotes que ainda precisam ser aprovados.)
