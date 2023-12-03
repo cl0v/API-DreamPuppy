7ab547daf5de56e623298e72ac93531f75224bfe
Marcelo Viana <marcelofv12@hotmail.com>
2023-12-03 15:39:26 -0300

    add test to ide

--- gallery/feat/puppy/exceptions.py
+++ gallery/feat/puppy/exceptions.py
@@ -3,3 +3,8 @@ class PuppyDetailsException(Exception):
         self.message = message
         self.status_code = status_code
 
+
+class PuppyStorageException(Exception):
+    def __init__(self, message: str, status_code: int):
+        self.message = message
+        self.status_code = status_code
