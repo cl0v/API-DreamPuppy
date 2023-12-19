132d2b204dcf6e771902e019e2ea4749450a435c
Marcelo Viana <marcelofv12@hotmail.com>
2023-12-19 13:29:04 -0300

    commenting all aws impl

--- app/feat/puppy/s3_storage.py
+++ app/feat/puppy/s3_storage.py
@@ -1,35 +1,32 @@
-# TODO: Remover
+# import boto3
+
+# from . import exceptions
+# from fastapi import UploadFile
+
+# client = boto3.client("s3")
+# bucket = "viana.dev2.gallery"
 
 
-import boto3
-
-from . import exceptions
-from fastapi import UploadFile
-
-client = boto3.client("s3")
-bucket = "viana.dev2.gallery"
-
-
-def get_url_by_key(puppy_uuid: str, media_uuid: str):
-    key = "{0}/{1}".format(puppy_uuid, media_uuid)
-    url = client.generate_presigned_url(
-        ClientMethod="get_object",
-        Params={"Bucket": bucket, "Key": key},
-        ExpiresIn=3600,
-    )
-    return url
+# def get_url_by_key(puppy_uuid: str, media_uuid: str):
+#     key = "{0}/{1}".format(puppy_uuid, media_uuid)
+#     url = client.generate_presigned_url(
+#         ClientMethod="get_object",
+#         Params={"Bucket": bucket, "Key": key},
+#         ExpiresIn=3600,
+#     )
+#     return url
 
 
-def upload_img(puppy_uuid: str, media_uuid: str, media: UploadFile):
-    if media.content_type == "application/octet-stream":
-        raise exceptions.PuppyStorageException(status_code=500, message="Erro ao salvar imagem")
-    key = "{0}/{1}".format(puppy_uuid, media_uuid)
-    client.upload_fileobj(
-        media.file,
-        bucket,
-        key,
-        ExtraArgs={
-            "ContentType": media.content_type,
-            "ACL": "public-read",
-        },
-    )
+# def upload_img(puppy_uuid: str, media_uuid: str, media: UploadFile):
+#     if media.content_type == "application/octet-stream":
+#         raise exceptions.PuppyStorageException(status_code=500, message="Erro ao salvar imagem")
+#     key = "{0}/{1}".format(puppy_uuid, media_uuid)
+#     client.upload_fileobj(
+#         media.file,
+#         bucket,
+#         key,
+#         ExtraArgs={
+#             "ContentType": media.content_type,
+#             "ACL": "public-read",
+#         },
+#     )
