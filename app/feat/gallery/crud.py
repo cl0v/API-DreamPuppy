73e7612d55b9d91941068c3bf313869dfe75bb2b
Marcelo Viana <marcelofv12@hotmail.com>
2024-02-25 12:17:58 -0300

    Adicionado sistema de paginação

--- app/feat/gallery/crud.py
+++ app/feat/gallery/crud.py
@@ -1,10 +1,10 @@
-from . import schemas, exceptions
-from fastapi import status
+from . import schemas#, exceptions
+# from fastapi import status
 from sqlalchemy.orm import Session
 from app.feat.puppy import models as puppy_models
 from app.feat.puppy.image_storage import get_gallery_image_url
-
-
+from fastapi_pagination.ext.sqlalchemy import paginate
+from fastapi_pagination import Page
 
 LIMIT_AMOUNT = 30
 # Ja foi revisado pelos moderadores
@@ -13,16 +13,17 @@ REVIEWED_DEFAULT = True
 VISIBLE_DEFAULT = True
 
 
-def fill_gallery(db: Session, amount: int = 9) -> list[schemas.GallerySchema]:
-    if amount > LIMIT_AMOUNT:
-        raise exceptions.GalleryException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            message="Too many items",
-        )
+def fill_gallery(db: Session) -> Page[schemas.GallerySchema]:
+    # TODO: Adicionar limitador de itens
+    # if amount > LIMIT_AMOUNT:
+    #     raise exceptions.GalleryException(
+    #         status_code=status.HTTP_400_BAD_REQUEST,
+    #         message="Too many items",
+    #     )
     # select puppy,uuid from medias where puppy in (SELECT id FROM puppies WHERE puppies.reviewed=True AND puppies.public_access=True);
-    
+
     # SELECT id FROM puppies WHERE puppies.reviewed=True AND puppies.public_access=True
-    
+
     q = (
         db.query(
             puppy_models.Media.puppy,
@@ -41,17 +42,21 @@ def fill_gallery(db: Session, amount: int = 9) -> list[schemas.GallerySchema]:
             puppy_models.Media.puppy,
             puppy_models.Media.uuid,
             puppy_models.PuppyModel.uuid,
-        ).distinct(puppy_models.Media.puppy)
-        .limit(amount)
-        .all()
+        )
+        .distinct(puppy_models.Media.puppy)
+        # .limit(amount)
+        # .all()
+    )
+    val = paginate(
+        db,
+        q,
+        transformer=lambda q: [
+            {
+                "id": id,
+                "url": get_gallery_image_url(media_uuid),
+            }
+            for id, media_uuid in q
+        ],
     )
 
-    result = [
-        {
-            "id": id,
-            "url": get_gallery_image_url(media_uuid),
-        }
-        for id, media_uuid in q
-    ]
-
-    return result
+    return val
